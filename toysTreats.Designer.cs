72ccd8a8ffda0abda668ee038fa40612c09808d6
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2024-03-27 22:37:58 -0400

    Set ver 6

---
+++ toysTreats.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace setJunior
+{
+    partial class toysTreats
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(254, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(298, 395);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(577, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 134);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Buy!";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 450);
+            this.label1.TabIndex = 5;
+            // 
+            // toysTreats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Name = "toysTreats";
+            this.Text = "toysTreats";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ toysTreats.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace setJunior
+{
+    public partial class toysTreats : Form
+    {
+        List<string> prices = new List<string>();
+        List<string> names = new List<string>();
+        public toysTreats()
+        {
+            names.Add("fish treat");
+            names.Add("alien treat");
+            names.Add("wind up mouse");
+            names.Add("alien squeaky toy");
+            prices.Add("25");
+            prices.Add("25");
+            prices.Add("50");
+            InitializeComponent();
+            listView1.View = View.Details;
+            for (int i = 0; i < names.Count; i++)
+            {
+                ListViewItem item = new ListViewItem(names[i]);
+                item.SubItems.Add(prices[i]);
+                listView1.Items.Add(item);
+            }
+            listView1.Columns.Add("Name", -1, HorizontalAlignment.Left);
+            listView1.Columns.Add("Cost", -2, HorizontalAlignment.Right);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (listView1.SelectedItems.ToString())
+            {
+                case "fish treat":
+                    label1.Text = "Decrease tiredness by 20 points";
+                    break;
+                case "wind up mouse":
+                    label1.Text = "Increase happiness by 20 points";
+                    break;
+                case "alien squeaky toy":
+                    label1.Text = "Doubles happiness";
+                    break;
+                case "alien treat":
+                    label1.Text = "Halves tiredness";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
